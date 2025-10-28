d82817ec42687d4f02ad86b38728b37b7b0993db
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-10-28 14:50:22 -0500

    fix: visualizer link

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,12 +1,19 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import type { ReactNode } from 'react';
-import { baseOptions } from '@/app/layout.config';
-import { source } from '@/lib/source';
+import {DocsLayout} from 'fumadocs-ui/layouts/docs';
+import type {ReactNode} from 'react';
+import {baseOptions} from '@/app/layout.config';
+import {source} from '@/lib/source';
+import Discord from "@/app/Discord";
+import {GithubInfo} from "fumadocs-ui/components/github-info";
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
-    </DocsLayout>
-  );
+export default function Layout({children}: { children: ReactNode }) {
+    return (
+        <DocsLayout tree={source.pageTree} {...baseOptions} links={[{
+            type: 'custom',
+            children: (
+                <GithubInfo owner="Pedro-Pathing" repo="PedroPathing" className="lg:-mx-2"/>
+            )
+        }, ...baseOptions.links!]}>
+            {children}
+        </DocsLayout>
+    );
 }
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,7 +1,8 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { GithubInfo } from "fumadocs-ui/components/github-info";
+import type {BaseLayoutProps, LinkItemType} from 'fumadocs-ui/layouts/shared';
+import {GithubInfo} from "fumadocs-ui/components/github-info";
 import Discord from "@/app/Discord";
-import Visualizer from './VisualizerIcon';
+import {NavigationMenuItem} from "fumadocs-ui/components/ui/navigation-menu";
+
 /**
  * Shared layout configurations
  *
@@ -10,37 +11,37 @@ import Visualizer from './VisualizerIcon';
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
-  nav: {
-    title: (
-      <>
-        <img className="size-10 dark:hidden" src={`${process.env.BASE_PATH}/logo-light.png`} alt="Pedro Pathing Logo" />
-        <img className="size-10 hidden dark:inline" src={`${process.env.BASE_PATH}/logo.png`} alt="Pedro Pathing Logo" />
+    nav: {
+        title: (
+            <>
+                <img className="size-10 dark:hidden" src={`${process.env.BASE_PATH}/logo-light.png`}
+                     alt="Pedro Pathing Logo"/>
+                <img className="size-10 hidden dark:inline" src={`${process.env.BASE_PATH}/logo.png`}
+                     alt="Pedro Pathing Logo"/>
 
-        <span>Pedro Pathing</span>
-      </>
-    ),
-  },
-  // see https://fumadocs.dev/docs/ui/navigation/links
-  links: [
-    {
-      text: "Visualizer",
-      url: "https://visualizer.pedropathing.com/",
-      icon: <Visualizer />,
-      type: 'icon'
+                <span>Pedro Pathing</span>
+            </>
+        ),
     },
-    {
-      type: 'custom',
-      children: (
-        <GithubInfo owner="Pedro-Pathing" repo="PedroPathing" className="lg:-mx-2" />
-      ),
-    },
-    {
-      text: "Discord",
-      url: "https://discord.gg/2GfC4qBP5s",
-      icon: <Discord />,
-      type: 'icon'
-    },
-
-  ],
-  githubUrl: "https://github.com/Pedro-Pathing/Docs"
+    // see https://fumadocs.dev/docs/ui/navigation/links
+    links: [
+        {
+            text: "Discord",
+            url: "https://discord.gg/2GfC4qBP5s",
+            icon: <Discord/>,
+            type: 'icon'
+        },
+        {
+            text: "Visualizer",
+            url: "https://visualizer.pedropathing.com",
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"/>
+                </svg>
+            )
+        },
+    ],
+    githubUrl: "https://github.com/Pedro-Pathing/Docs"
 };
