4aa7768a9eb992b9020eeff60bb5a8ce0e3d0443
nesetkab <foulofdoom@gmail.com>
2025-10-28 12:24:01 -0600

    add visualizer icon & link

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,7 +1,7 @@
-import type {BaseLayoutProps} from 'fumadocs-ui/layouts/shared';
-import {GithubInfo} from "fumadocs-ui/components/github-info";
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { GithubInfo } from "fumadocs-ui/components/github-info";
 import Discord from "@/app/Discord";
-
+import Visualizer from './VisualizerIcon';
 /**
  * Shared layout configurations
  *
@@ -10,30 +10,37 @@ import Discord from "@/app/Discord";
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
-    nav: {
-        title: (
-            <>
-                <img className="size-10 dark:hidden" src={`${process.env.BASE_PATH}/logo-light.png`} alt="Pedro Pathing Logo"/>
-                <img className="size-10 hidden dark:inline" src={`${process.env.BASE_PATH}/logo.png`} alt="Pedro Pathing Logo"/>
+  nav: {
+    title: (
+      <>
+        <img className="size-10 dark:hidden" src={`${process.env.BASE_PATH}/logo-light.png`} alt="Pedro Pathing Logo" />
+        <img className="size-10 hidden dark:inline" src={`${process.env.BASE_PATH}/logo.png`} alt="Pedro Pathing Logo" />
 
-                <span>Pedro Pathing</span>
-            </>
-        ),
+        <span>Pedro Pathing</span>
+      </>
+    ),
+  },
+  // see https://fumadocs.dev/docs/ui/navigation/links
+  links: [
+    {
+      text: "Visualizer",
+      url: "https://visualizer.pedropathing.com/",
+      icon: <Visualizer />,
+      type: 'icon'
     },
-    // see https://fumadocs.dev/docs/ui/navigation/links
-    links: [
-        {
-            type: 'custom',
-            children: (
-                <GithubInfo owner="Pedro-Pathing" repo="PedroPathing" className="lg:-mx-2"/>
-            ),
-        },
-        {
-            text: "Discord",
-            url: "https://discord.gg/2GfC4qBP5s",
-            icon: <Discord/>,
-            type: 'icon'
-        }
-    ],
-    githubUrl: "https://github.com/Pedro-Pathing/Docs"
+    {
+      type: 'custom',
+      children: (
+        <GithubInfo owner="Pedro-Pathing" repo="PedroPathing" className="lg:-mx-2" />
+      ),
+    },
+    {
+      text: "Discord",
+      url: "https://discord.gg/2GfC4qBP5s",
+      icon: <Discord />,
+      type: 'icon'
+    },
+
+  ],
+  githubUrl: "https://github.com/Pedro-Pathing/Docs"
 };
